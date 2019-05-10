2fe4ac6cbb496b3f2281fcbbffe293341716fd2d
Aleksandra Shumkoska <ashumkoska@sevengates.co>
2019-05-10 08:12:52 +0200

    [IMP] remove access link from emails

--- models/mail.py
+++ models/mail.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from openerp import SUPERUSER_ID
 from openerp import api, models, fields, _
 
 
@@ -13,3 +14,15 @@ class mail_mail(models.Model):
     @api.depends('body_html')
     def compute_body_c_html(self):
         self.body_c_html = self.body_html
+        
+    def _get_partner_access_link(self, cr, uid, mail, partner=None, context=None):
+        ''' Generate URLs for links in mails:
+            - partner is not an user: signup_url
+            - partner is an user: fallback on classic URL
+        '''
+        if context is None:
+            context = {}
+        if partner and not partner.user_ids:
+            return ''
+        else:
+            return super(mail_mail, self)._get_partner_access_link(cr, uid, mail, partner=partner, context=context)
